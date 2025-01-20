a8f73e889d06a10be5ae2d08fcf346597cbaf080
Subhransu Sekhar Mishra <msubhransu1@gmail.com>
2025-01-20 22:37:14 +0530

    Set up and intallations

---
+++ client/src/App.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import Home from './pages/Home'
+import BuyCredit from './pages/BuyCredit'
+import Result from './pages/Result'
+
+const App = () => {
+  return (
+    <div>
+      <Home/>
+      <BuyCredit/>
+      <Result/>
+      
+    </div>
+  )
+}
+
+export default App
