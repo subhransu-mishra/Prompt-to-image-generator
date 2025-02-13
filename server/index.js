4504675b55d23cca04885af1847aed1d3b924d5d
Subhransu Sekhar Mishra <msubhransu1@gmail.com>
2025-02-14 00:09:17 +0530

    cors and backend URL updated

--- server/index.js
+++ server/index.js
@@ -8,8 +8,12 @@ import imageRouter from "./routes/imageRouter.js";
 
 const PORT = process.env.PORT || 4000;
 const app = express();
-
-app.use(cors())
+app.use(cors({
+    origin: 'https://canvas-ai-4-git-main-subhransu-sekhar-mishras-projects.vercel.app',
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    credentials: true
+}));
+// app.use(cors())
 app.use(express.json())
 await connectDB()
 
